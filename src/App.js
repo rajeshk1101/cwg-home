fc71f81f0f91f6f3e84d9c40a062d3a9f7a044a8
Rajesh K <rajeshk1101@gmail.com>
2023-07-12 07:43:49 +0530

    Value change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
   return (<><div>Welcome to cwg portal</div><a href="https://cabinetworksgroup--itedev.sandbox.my.site.com/test">Click here to login to salesforce</a></>);
 }
 
