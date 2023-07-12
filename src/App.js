2b023c5beb312e86efa9bd14e5acee293f700474
Rajesh K <rajeshk1101@gmail.com>
2023-07-12 10:42:18 +0530

    Nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
 function App() {
-  return (<><div>Welcome to cwg portal</div><a href="https://cabinetworksgroup--itedev.sandbox.my.site.com/test">Click here to login to salesforce</a></>);
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+      </Routes>
+    </Router>
+  );
+  //return (<><div>Welcome to cwg portal</div><a href="https://cabinetworksgroup--itedev.sandbox.my.site.com/test">Click here to login to salesforce</a></>);
 }
 
 export default App;
