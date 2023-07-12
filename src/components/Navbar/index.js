4715251d518cbd8d6950fdfa45c7f1e56dd690f8
Rajesh K <rajeshk1101@gmail.com>
2023-07-12 17:51:46 +0530

    Nav fix Hook

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,41 +7,40 @@ NavMenu,
 NavBtn,
 NavBtnLink,
 } from './NavbarElements';
-const url = "/.auth/me";
-const [data, setData] = useState([]);
+const Navbar = () => {	
+	const url = "/.auth/me";
+	const [data, setData] = useState([]);
 
-const fetchInfo = () => {
-	return fetch(url)
-		.then((res) => res.json())
-		.then((d) => setData(d))
-}
-useEffect(() => {
-	fetchInfo();
-	callBackonEnd();
-	}, []);
-const Navbar = () => {
-return (
-	<>
-	<Nav>
-		<Bars />
-		<NavMenu>
-			<NavLink to='/' activeStyle>
-				Home
-			</NavLink>
-			<NavLink to='/events' activeStyle>
-				Events
-			</NavLink>
-			{/* Second Nav */}
-		</NavMenu>
-		<NavBtn>
-			<NavBtnLink to="/signin">CWG Community</NavBtnLink>
-			<NavBtnLink to='/logout'>Logout</NavBtnLink> 
-		</NavBtn>
-	</Nav>
-	</>
-);
+	const fetchInfo = () => {
+		return fetch(url)
+			.then((res) => res.json())
+			.then((d) => setData(d))
+	}
+	useEffect(() => {
+		fetchInfo();
+		}, []);
+	return (
+		<>
+		<Nav>
+			<Bars />
+			<NavMenu>
+				<NavLink  activeStyle>
+					Welcome {data ? data.clientPrincipal ? data.clientPrincipal.userDetails : 'No Claim Priciple' : 'No data' }
+				</NavLink>
+				<NavLink to='/' activeStyle>
+					Home
+				</NavLink>
+				<NavLink to='/events' activeStyle>
+					Events
+				</NavLink>
+				{/* Second Nav */}
+			</NavMenu>
+			<NavBtn>
+				<NavBtnLink to="/signin">CWG Community</NavBtnLink>
+				<NavBtnLink to='/logout'>Logout</NavBtnLink> 
+			</NavBtn>
+		</Nav>
+		</>
+	);
 };
-function callBackonEnd(){
-	console.log('On Nav '+ JSON.stringify(data));
-}
 export default Navbar;
