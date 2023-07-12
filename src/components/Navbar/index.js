f37649ff1668ac7f438a929b85f334aa81d02075
Rajesh K <rajeshk1101@gmail.com>
2023-07-12 11:25:07 +0530

    Nav fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -15,15 +15,6 @@ return (
 		<Bars />
 
 		<NavMenu>
-		<NavLink to='/about' activeStyle>
-			About
-		</NavLink>
-		<NavLink to='/events' activeStyle>
-			Events
-		</NavLink>
-		<NavLink to='/sign-up' activeStyle>
-			Sign Up
-		</NavLink>
 		{/* Second Nav */}
 		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
 		</NavMenu>
