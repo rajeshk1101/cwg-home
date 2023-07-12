30b3c29ab8845f8ab75a6867de2195b0d7d0ff8c
Rajesh K <rajeshk1101@gmail.com>
2023-07-12 07:40:03 +0530

    Auth Changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return (<><div>Hello {value}</div><a href="https://cabinetworksgroup--itedev.sandbox.my.site.com/test">Click here to login to salesforce</a></>);
+  return (<><div>Welcome to cwg portal</div><a href="https://cabinetworksgroup--itedev.sandbox.my.site.com/test">Click here to login to salesforce</a></>);
 }
 
 export default App;
